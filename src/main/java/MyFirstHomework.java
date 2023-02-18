58221351e75ede94d64cb4b2a889e389b2faa199
Gennadiy <gehna@yandex.ru>
2023-02-18 17:49:15 +0300

    Update MyFirstHomework.java
    Added !!!! marks

--- src/main/java/MyFirstHomework.java
+++ src/main/java/MyFirstHomework.java
@@ -1,5 +1,5 @@
 public class MyFirstHomework {
     public static void main(String[] args) {
-        System.out.println("Hello message new-branch");
+        System.out.println("Hello message new-branch!!!!!!");
     }
 }
