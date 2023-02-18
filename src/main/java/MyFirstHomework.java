b5d6c92d679947480b497853656d29fc8ed7937a
Gennadiy <gehna@yandex.ru>
2023-02-18 17:50:34 +0300

    Update MyFirstHomework.java
    added 'msg' variable

--- src/main/java/MyFirstHomework.java
+++ src/main/java/MyFirstHomework.java
@@ -1,5 +1,8 @@
 public class MyFirstHomework {
+    
+    private static String msg = "Hello message new-branch!!!!!!";
+    
     public static void main(String[] args) {
-        System.out.println("Hello message new-branch!!!!!!");
+        System.out.println(msg);
     }
 }
