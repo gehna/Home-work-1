abac6f67a305e463d57d838de3e9dd67eabfac61
Legkodukhov Gennadiy <gehna@yandex.ru>
2023-02-18 17:51:27 +0300

    Added 'getMessage' method

--- src/main/java/MyFirstHomework.java
+++ src/main/java/MyFirstHomework.java
@@ -1,5 +1,9 @@
 public class MyFirstHomework {
     public static void main(String[] args) {
-        System.out.println("Hello message new-branch");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage(){
+        return "Hello message";
     }
 }
